11cddbe49a67ccbe87dfc95fb73f89663a87ed4a
VanceGoblin <zvkubajak@gmail.com>
2024-09-17 18:55:03 -0500

    Added Route Path Functionality

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import apiRoutes from './api/index.js';
-// import { authenticateToken } from '../middleware/auth.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
-// TODO: Add authentication to the API routes
-// TODO we will only want to authenticate the routes for the home page (search, save books etc.)
-// router.use(authenticateToken);
-router.use('/api', apiRoutes);
+router.use('/api/login', apiRoutes);
+// The login route should not use authentication because users need to log in to be authenticated.
+
+router.use('/api/', authenticateToken, apiRoutes);
 
 export default router;
