f5103c85ba584ccdec52758b377eb97e8b27378c
VanceGoblin <zvkubajak@gmail.com>
2024-09-17 20:13:53 -0500

    Added CSS Folders

---
+++ client/src/pages/Reviews.tsx
@@ -0,0 +1,7 @@
+import './css/Reviews.css';
+
+export default function Reviews() {
+  return (
+    <h1>Reviews</h1>
+  );
+}
---
+++ client/src/pages/Search.tsx
@@ -0,0 +1,7 @@
+import './css/Search.css';
+
+export default function Search() {
+  return (
+    <h1>Search</h1>
+  );
+}
