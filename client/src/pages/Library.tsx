a980fc8419a09ada298a8af02d7f7e1749a3efbb
VanceGoblin <zvkubajak@gmail.com>
2024-09-20 11:05:51 -0500

    Added Optional Test Values and Basic Bootstrap Styled CSS

--- client/src/pages/Library.tsx
+++ client/src/pages/Library.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Book } from "../utils/bookInterface.ts";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 import "./css/Library.css";
 
 const Library = () => {
@@ -8,6 +10,31 @@ const Library = () => {
 
     return savedBooks ? JSON.parse(savedBooks) : [];
   });
+
+  // TEST VALUES //
+
+  /*
+  const [books, setBooks] = useState<Book[]>([
+    {
+      bookId: 1,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      isbn: "9780743273565",
+    },
+    {
+      bookId: 2,
+      title: "1984",
+      author: "George Orwell",
+      isbn: "9780451524935",
+    },
+    {
+      bookId: 3,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      isbn: "9780061120084",
+    },
+  ]);
+  */
 
   const removeBook = (index: number) => {
     const updatedBooks = books.filter((_, i) => i !== index);
@@ -19,35 +46,36 @@ const Library = () => {
   books.forEach((book, index) => {
     rows.push(
       <tr>
-        <td>{book.bookId}</td>
+        <td className="center">{book.bookId}</td>
         <td>{book.title}</td>
         <td>{book.author}</td>
         <td>{book.isbn}</td>
-        <td>
-          <button id="remove-book" onClick={() => removeBook(index)}>
+        <td className="center">
+          <Button variant="danger" onClick={() => removeBook(index)}>
             Remove
-          </button>
+          </Button>
         </td>
       </tr>
     );
   });
 
   return (
-    <div id="library-container">
+    <div id="library">
       <h1>Library</h1>
 
       <main>
-        <table>
+        <Table striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th>#</th>
+              <th className="center">#</th>
               <th>Title</th>
               <th>Author</th>
               <th>ISBN</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>{rows}</tbody>
-        </table>
+        </Table>
       </main>
     </div>
   );
