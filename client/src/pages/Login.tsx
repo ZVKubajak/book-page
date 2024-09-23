2b0b549ffc3ee3fdf942bc1f8df3176de3402238
@selmgar-eEHSZr <selmgar@gmail.com>
2024-09-22 20:28:19 -0500

    Adds form validation to the login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,19 +5,41 @@ import { loginUser } from "../api/userAPI";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [nameIsEmpty, setNameIsEmpty] = useState(false);
+  const [disableSubmit, setDisableSubmit] = useState(true);
   const [loginData, setLoginData] = useState({
     username: '',
     password: ''
   });
+  const [passwordisEmpty, setPasswordIsEmpty] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setLoginData({
+    const updatedLoginData = {
       ...loginData,
       [name]: value
-    });
+    }
+    setLoginData(updatedLoginData);
+    
+    // Update disableSubmit state based on the updatedLoginData
+    setDisableSubmit(
+      updatedLoginData.username.trim() === '' || updatedLoginData.password.trim() === ''
+    );
   };
-const navigate = useNavigate();
+
+  const handleBlur = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    if (name === 'username') {
+      setNameIsEmpty(value.trim() === '');
+    }
+    
+    if (name === 'password') {
+      setPasswordIsEmpty(value.trim() === '');
+    }
+    
+  };
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -41,19 +63,31 @@ const navigate = useNavigate();
           name='username'
           value={loginData.username || ''}
           onChange={handleChange}
+          onBlur={handleBlur}
           id="username"
-          className="form-control mb-4"
+          className={`form-control border ${nameIsEmpty ? 'border-danger' : ''}`}
         />
-        <label className='form-label'>Password</label>
+        {nameIsEmpty && 
+        	<div className='mt-2 text-danger'>
+          	Name is required.
+        	</div>
+        }
+        <label className='form-label mt-4'>Password</label>
         <input 
           type='password'
           name='password'
           value={loginData.password || ''}
           onChange={handleChange}
           id="password"
-          className="form-control"
+          className={`form-control border ${passwordisEmpty ? 'border-danger' : ''}`}
+          onBlur={handleBlur}
         />
-        <button className="btn btn-outline-primary my-4" type='submit'>Submit</button>
+        {passwordisEmpty && 
+        	<div className="mt-2 text-danger">
+          	Password is required.
+        	</div>
+        }
+        <button disabled={disableSubmit} className="btn btn-outline-primary my-4" type='submit'>Submit</button>
       </form>
       <div>
         <Link className="link-offset-3" to="/signup">Sign Up</Link>
